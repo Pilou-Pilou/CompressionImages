e5043f0f0cf7a2dc8b1ab4e3cb79ed0eef425f01
Sergent Julien <sergent.julien.1@gmail.com>
2015-11-05 08:49:45 +0100

    Add the creation of segment and writing file

--- src/algo/IterativeCompression.java
+++ src/algo/IterativeCompression.java
@@ -2,6 +2,12 @@ package algo;
 
 import data.Data;
 import data.Value;
+import file.BitOutputStream;
+
+import javax.xml.crypto.OctetStreamData;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * <b>File :</b> IterativeCompression.java<br>
@@ -18,7 +24,7 @@ public class IterativeCompression {
     /**
      * Constructor of the class IterativeCompression
      */
-    public IterativeCompression(){
+    public IterativeCompression() throws IOException {
 
         value = new Value[Data.arrayOfByte.length];
 
@@ -29,7 +35,26 @@ public class IterativeCompression {
         for(int i= Data.arrayOfByte.length-1; i>=0 ; i--)
             countMin(i);
 
+        System.out.println("----Segment-Start---");
         // create the sequence and put it in the file
+        int idMax,nbBiteUse;
+        BitOutputStream a = new BitOutputStream(new FileOutputStream("tt.raw"));
+        for(int i=0;i<value.length;i++){
+
+            idMax = value[i].getLengthOfSegment()+i;
+            nbBiteUse = value[i].getNbBitUse();
+            Segment segment = new Segment(value[i].getLengthOfSegment(),nbBiteUse);
+            System.out.println(i+" "+idMax);
+            while(i < idMax){
+                segment.fillBodySegement(Data.arrayOfByte[i]);
+                i++;
+            }
+            a.write(segment);
+            // for don't have twice i++ (in while and with for)
+            i--;
+        }
+        a.close();
+        System.out.println("----Segment-End---");
     }
 
     /**
@@ -95,39 +120,44 @@ public class IterativeCompression {
             value[index].setLengthOfSegment(1);
             value[index].setNbBitUse(nbBitUse);
         }else {
-            if(nbBitUse>value[index-1].getNbBitUse()) {
-                // 0111 0010 0011
-                tmp1 = value[index - 1].getCost() + 11 + nbBitUse;
-                tmp2 = value[index - 1].getCost() + nbBitUse + value[index - 1].getLengthOfSegment() * (nbBitUse - value[index - 1].getNbBitUse());
-                min = Math.min(tmp1,tmp2);
-                if(min == tmp1){
-                    value[index].setLengthOfSegment(1);
-                    value[index].setNbBitUse(nbBitUse);
-                }else{
-                    value[index].setLengthOfSegment(value[index - 1].getLengthOfSegment()+1);
-                    value[index].setNbBitUse(nbBitUse);
-                }
-            }else if(nbBitUse == value[index-1].getNbBitUse()) {
-                // un seul choix possible car on ne peut pas prendre moin de bit et si on prend plus de bit sa nous coutera plus chére
-                tmp2 = value[index - 1].getCost() + value[index - 1].getNbBitUse();
-                min = tmp2;
-                value[index].setLengthOfSegment(value[index - 1].getLengthOfSegment()+1);
+            if(value[index+1].getLengthOfSegment() >= 256){
+                min = 11 + nbBitUse;
+                value[index].setLengthOfSegment(1);
                 value[index].setNbBitUse(nbBitUse);
             }else {
-                // 0011  0111  0101
-                tmp1 = value[index - 1].getCost() + 11 + nbBitUse;
-                tmp2 = value[index - 1].getCost() + value[index - 1].getNbBitUse();
-                min = Math.min(tmp1,tmp2);
-                if(min == tmp1){
-                    value[index].setLengthOfSegment(1);
+                if (nbBitUse > value[index + 1].getNbBitUse()) {
+                    // 0111 0010 0011
+                    tmp1 = value[index + 1].getCost() + 11 + nbBitUse;
+                    tmp2 = value[index + 1].getCost() + nbBitUse + value[index + 1].getLengthOfSegment() * (nbBitUse - value[index + 1].getNbBitUse());
+                    min = Math.min(tmp1, tmp2);
+                    if (min == tmp1) {
+                        value[index].setLengthOfSegment(1);
+                        value[index].setNbBitUse(nbBitUse);
+                    } else {
+                        value[index].setLengthOfSegment(value[index + 1].getLengthOfSegment() + 1);
+                        value[index].setNbBitUse(nbBitUse);
+                    }
+                } else if (nbBitUse == value[index + 1].getNbBitUse()) {
+                    // un seul choix possible car on ne peut pas prendre moin de bit et si on prend plus de bit sa nous coutera plus chére
+                    tmp2 = value[index + 1].getCost() + value[index + 1].getNbBitUse();
+                    min = tmp2;
+                    value[index].setLengthOfSegment(value[index + 1].getLengthOfSegment() + 1);
                     value[index].setNbBitUse(nbBitUse);
-                }else{
-                    value[index].setLengthOfSegment(value[index - 1].getLengthOfSegment()+1);
-                    value[index].setNbBitUse(value[index - 1].getNbBitUse());
+                } else {
+                    // 0011  0111  0101
+                    tmp1 = value[index + 1].getCost() + 11 + nbBitUse;
+                    tmp2 = value[index + 1].getCost() + value[index + 1].getNbBitUse();
+                    min = Math.min(tmp1, tmp2);
+                    if (min == tmp1) {
+                        value[index].setLengthOfSegment(1);
+                        value[index].setNbBitUse(nbBitUse);
+                    } else {
+                        value[index].setLengthOfSegment(value[index + 1].getLengthOfSegment() + 1);
+                        value[index].setNbBitUse(value[index + 1].getNbBitUse());
+                    }
                 }
             }
         }
-
         value[index].setCost(min);
         return min;
     }
